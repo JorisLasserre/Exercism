d378543100ccbe1cb72c450b4d54a018a6162927
JorisLasserre <jl.jorislasserre@gmail.com>
2025-02-06 18:45:14 +0100

    more elegant version

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,44 +1,9 @@
-
+import java.util.Arrays;
+import java.util.List;
 
 class ResistorColorDuo {
+    final List<String> codes = Arrays.asList("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white");
     int value(String[] colors) {
-        String resistorValue = "";
-        for(int i=0; i<2; i++){
-            String color = colors[i];
-            switch (color){
-                case "black":
-                    resistorValue += "0";
-                    break;
-                case "brown":
-                    resistorValue += "1";
-                    break;
-                case "red":
-                    resistorValue += "2";
-                    break;
-                case "orange":
-                    resistorValue += "3";
-                    break;
-                case "yellow":
-                    resistorValue += "4";
-                    break;
-                case "green":
-                    resistorValue += "5";
-                    break;
-                case "blue":
-                    resistorValue += "6";
-                    break;
-                case "violet":
-                    resistorValue += "7";
-                    break;
-                case "grey":
-                    resistorValue += "8";
-                    break;
-                case "white":
-                    resistorValue += "9";
-                    break;
-            }
-        }
-        int resistorValueNumber = Integer.parseInt(resistorValue);
-        return resistorValueNumber;
+        return codes.indexOf(colors[0]) * 10 + codes.indexOf(colors[1]);
     }
 }
